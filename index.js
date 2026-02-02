8422b46eb2236131bc337d798ac9c08caa20bd0f
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-02-02 14:01:31 +0000

    Add some error handling

--- index.js
+++ index.js
@@ -14,39 +14,48 @@ const server = http.createServer(async (req, res) => {
     let statusCode = 200;
     let data = null;
 
-    switch (req.url) {
-        case "/": {
-            if (!index) {
-                index = await fs.readFile("./index.html");
+    try {
+        switch (req.url) {
+            case "/": {
+                if (!index) {
+                    index = await fs.readFile("./index.html");
+                }
+                data = index;
+                break;
             }
-            data = index;
-            break;
-        }
-        case "/about": {
-            if (!about) {
-                about = await fs.readFile("./about.html");
+            case "/about": {
+                if (!about) {
+                    about = await fs.readFile("./about.html");
+                }
+                data = about;
+                break;
             }
-            data = about;
-            break;
-        }
-        case "/contact-me": {
-            if (!contact) {
-                contact = await fs.readFile("./contact-me.html");
+            case "/contact-me": {
+                if (!contact) {
+                    contact = await fs.readFile("./contact-me.html");
+                }
+                data = contact;
+                break;
             }
-            data = contact;
-            break;
-        }
-        default: {
-            if (!err404) {
-                err404 = await fs.readFile("./404.html");
+            default: {
+                if (!err404) {
+                    err404 = await fs.readFile("./404.html");
+                }
+                statusCode = 404;
+                data = err404;
             }
-            statusCode = 404;
-            data = err404;
         }
+    } catch (err) {
+        console.log(err);
+        if (!err404) {
+            err404 = await fs.readFile("./404.html");
+        }
+        statusCode = 404;
+        data = err404;
+    } finally {
+        res.writeHead(statusCode, headers);
+        res.end(data);
     }
-
-    res.writeHead(statusCode, headers);
-    res.end(data);
 });
 
 server.listen(8080);
